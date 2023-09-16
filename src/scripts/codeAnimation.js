9d1cef698fdc2ed62fb45373fbfa3acb841a0882
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-09-16 15:09:03 +0900

    コードをマシに

--- src/scripts/codeAnimation.js
+++ src/scripts/codeAnimation.js
@@ -1,26 +1,22 @@
-class codeAnime {
-    constructor(element) {
-        const bodyCode = element.innerHTML.split("");
-        let visualCode = "";
-        let init = 0;
-        const addCode = () => {
-            if (init >= bodyCode.length) {
-                cancelAnimationFrame(addCode);
-                return;
-            }
-            visualCode += bodyCode[init];
-            element.innerHTML = visualCode;
-            init++;
-            setTimeout(() => {requestAnimationFrame(addCode);},2.5)
-        };
-        addCode();
-    }
-}
+const setCodeAnimation = element => {
+    const mainCode = Array.from(element.innerHTML)
 
+    let tmpCode = ''
+
+    let index = 0
+    const next = () => {
+        if (init < bodyCode.length) {
+            tmpCode += mainCode[index]
+            element.innerHTML = tmpCode
+            requestAnimationFrame(next)
+        }
+    }
+    next()
+}
 function callbackBuild() {
     let divs = document.querySelectorAll("body > div,box");
 
     for (let i = 0; i < divs.length;i++) {
-        new codeAnime(divs[i])
+        setCodeAnimation(divs[i])
     }
 }
